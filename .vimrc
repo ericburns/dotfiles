c75a57778e34f2dcdaac2f37380c0002afc8b76e
Eric Burns <ericjosephburns@gmail.com>
2016-08-31 21:56:20 +0000

    init

---
+++ .vimrc
@@ -0,0 +1,22 @@
+execute pathogen#infect()
+syntax enable
+filetype plugin indent on
+
+set wildignore+=*/tmp/*,*.so,*.swp,*.zip,*.pyc
+
+" Ctrlp
+let g:ctrlp_custom_ignore = '\v[\/]\.(git|hg|svn)$'
+
+" Editor
+set number
+set ts=4
+set sw=4
+set cursorline
+set autoindent
+set expandtab
+set showmatch
+
+let python_highlight_all = 1
+
+" Theme
+set background=dark
---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+BUNDLE_DIR=~/.vim/bundle
+AUTOLOAD_DIR=~/.vim/autoload
+
+echo Copying .vimrc to home directory
+cp .vimrc ~/.vimrc
+
+#
+# INSTALL PATHOGEN
+#
+echo Installing Pathogen
+[ ! -d $AUTOLOAD_DIR ] && mkdir -p $AUTOLOAD_DIR
+[ ! -d $BUNDLE_DIR ] && mkdir -p $BUNDLE_DIR
+[ ! -f $AUTOLOAD_DIR/pathogen.vim ] && curl -LSso $AUTOLOAD_DIR/pathogen.vim https://tpo.pe/pathogen.vim
+
+#
+# INSTALL PLUGINS
+#
+echo Installing plugins
+cd $BUNDLE_DIR
+
+# Sensible.vim
+echo Installing sensible.vim
+[ ! -d $BUNDLE_DIR/vim-sensible ] && git clone git://github.com/tpope/vim-sensible.git
+
+# Nerdtree
+echo Installing nerdtree
+[ ! -d $BUNDLE_DIR/nerdtree ] && git clone https://github.com/scrooloose/nerdtree.git
+
+# Ctrlp
+echo Installing ctrlp
+[ ! -d $BUNDLE_DIR/ctrlp.vim ] && git clone https://github.com/ctrlpvim/ctrlp.vim.git
+
+# Complete
+echo Vim setup complete
